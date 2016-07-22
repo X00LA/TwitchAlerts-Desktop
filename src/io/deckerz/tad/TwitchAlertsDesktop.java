b5f8ebbe5b52a23a24d2133ce148434e71ad45ff
BuildTools <unconfigured@null.spigotmc.org>
2016-07-22 18:02:49 +0100

    Code complete TODO - make notification pretty
    Had to include source JSON as maven is silly

--- src/io/deckerz/tad/TwitchAlertsDesktop.java
+++ src/io/deckerz/tad/TwitchAlertsDesktop.java
@@ -3,6 +3,9 @@ package io.deckerz.tad;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +13,7 @@ public class TwitchAlertsDesktop {
     static List<Thread> channels = new ArrayList<>();
     static Class TWD = TwitchAlertsDesktop.class;
     public static void main(String[] args) {
+        readConfig();
         TrayIcon trayIcon;
         if (SystemTray.isSupported()) {
             SystemTray tray = SystemTray.getSystemTray();
@@ -35,18 +39,33 @@ public class TwitchAlertsDesktop {
             System.exit(0);
         }
     }
-
     public static void exit() {
         for (int i = 0; i != channels.size(); i++) {
             channels.get(i).stop();
         }
         System.exit(0);
     }
-
     public static void start() {
         for (int i = 0; i != channels.size(); i++) {
             channels.get(i).start();
         }
     }
 
+    public static void readConfig() {
+        try {
+            String path = "./config.txt";
+            FileInputStream fis = new FileInputStream(path);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+
+                System.out.println(line);
+                channels.add(new Thread(new Checker(line)));
+            }
+            in.close();
+        } catch (Exception e) {
+            System.out.println(e.getStackTrace());
+            System.exit(0);
+        }
+    }
 }
